72e750917f23823ed60b2cddf881f729c92e2413
Varfed <49102674+Varfed@users.noreply.github.com>
2019-05-19 18:15:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -206,9 +206,9 @@ let prefix = prefixes[message.guild.id].prefixes;
    // if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Sorry pal, you can't do that.");
     let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     if(!rMember) return message.reply("Couldn't find that user, yo.");
-
-  
-    let gRole = message.guild.roles.find(`name`, "Создатель");
+    let role = args.join(" ").slice(22);
+    if(!role) return message.reply("Specify a role!");
+    let gRole = message.guild.roles.find(`name`, role);
     if(!gRole) return message.reply("Couldn't find that role.");
   
     if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
